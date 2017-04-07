28e1c13288d3246e4c56f2ab6d50de0fab4b1106
Imaizumi <zeal15300039@c-league.jp>
2017-04-07 20:41:28 +0900

    コミット忘れ　地面の追加

--- A/BaseCrossDx11/GameStage.h
+++ A/BaseCrossDx11/GameStage.h
@@ -18,8 +18,9 @@ namespace basecross
 		void CreateViewLight();
 		// 背景
 		void CreateBackGround();
+		void CreateGround();
 		void CreatePlate();
-
+		void CreatePlayerLin();
 
 	public:
 		//構築と破棄
@@ -33,6 +34,28 @@ namespace basecross
 		virtual void OnUpdate() override;
 
 
+	};
+
+	//地面
+	class Ground_GameStage : public GameObject
+	{
+	private:
+		//SP
+		Vector3 m_Scale;
+		Vector3 m_Positon;
+		wstring m_TX_Name;
+
+	public:
+		//コンストラクタ
+		Ground_GameStage(const shared_ptr<Stage>& StagePtr,
+			const Vector3& Scale,
+			const Vector3& Positon ,
+			const wstring& TX_Name);
+
+		//デストラクタ
+		virtual ~Ground_GameStage() {};
+
+		virtual void OnCreate() override;
 	};
 
 
